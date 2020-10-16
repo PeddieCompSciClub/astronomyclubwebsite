04103512d11d317721fd335441c466fe3c812455
Advaith Hari <33227909+advaithhari@users.noreply.github.com>
2020-10-16 18:00:46 -0400

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAsn9YRDAn3DgaH7uwVt9QnNuCl6CN9pqY",
+    authDomain: "astronomyclub-e7efc.firebaseapp.com",
+    databaseURL: "https://astronomyclub-e7efc.firebaseio.com",
+    projectId: "astronomyclub-e7efc",
+    storageBucket: "astronomyclub-e7efc.appspot.com",
+    messagingSenderId: "712574309846",
+    appId: "1:712574309846:web:69a5d6ad279065fc3b4077",
+    measurementId: "G-BWT11LH2FW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
